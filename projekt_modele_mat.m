a726fc7b7e383a8a4f3d92dd7d03b50306879e3d
Barbara Jurzysta <50555259+barbarajurzysta@users.noreply.github.com>
2019-12-30 15:22:01 +0100

    Update projekt_modele_mat.m
    Może ma sens

--- projekt_modele_mat.m
+++ projekt_modele_mat.m
@@ -4,16 +4,22 @@ function [Pr,T] = ProbRing2(N,a,b)
     Pr_curr = 1/N + zeros(N,1);
     diff=1;
     t=0;
-    while (all(diff>1e-10))
+    while (diff>1e-10)
       Pr_prev = Pr_curr;
-      Pr_curr(1)=
-      Pr_curr(N)=
-      f=@(i) a(i-1)*Pr_prev(i-1) + b*Pr_prev(i+1) - (b+a(i-1))*Pr_prev(i);
+      
+      h=0.5* (a(N)*Pr_prev(N) + b*Pr_prev(2) - (b+a(1))*Pr_prev(1));
+      Pr_curr(1)+= a(N)*Pr_prev(N) + b*Pr_prev(2) - (b+a(1))*(Pr_prev(1)+h);
+      
+      h=0.5* (a(N-1)*Pr_prev(N-1) + b*Pr_prev(1) - (b+a(N))*Pr_prev(N));
+      Pr_curr(N)+= a(N-1)*Pr_prev(N-1) + b*Pr_prev(1) - (b+a(N))*(Pr_prev(N)+h);
+      
+      f=@(i) a(i-1)*Pr_prev(i-1) + b*Pr_prev(i+1) - (b+a(i))*Pr_prev(i);
+      
       for i=2:(N-1)
-        h=0.5*
-        Pr_curr(i)=
+        h=0.5*f(i);
+        Pr_curr(i)+=a(i-1)*Pr_prev(i-1) + b*Pr_prev(i+1) - (b+a(i))*(Pr_prev(i)+h);
       end    
-      diff=abs(Pr_prev-Pr_curr)./Pr_prev;
+      diff=max(abs(Pr_prev-Pr_curr)./Pr_prev);
       t++;
     end
     Pr=Pr_curr;
